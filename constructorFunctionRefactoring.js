7298704abfc8fb22893df25849d20c1b73172edb
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2019-04-13 17:43:49 +0200

    Create constructorFunctionRefactoring.js
    Exercise from The Advanced Web Developer Bootcamp (Udemy Course)

---
+++ constructorFunctionRefactoring.js
@@ -0,0 +1,29 @@
+// REFACTOR BELOW CODE
+function Parent(firstName, lastName, favoriteColor, favoriteFood){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.favoriteColor = favoriteColor;
+    this.favoriteFood = favoriteFood;
+}
+
+function Child(firstName, lastName, favoriteColor, favoriteFood){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.favoriteColor = favoriteColor;
+    this.favoriteFood = favoriteFood;
+}
+
+// SOLUTION
+function Parent(firstName, lastName, favoriteColor, favoriteFood){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.favoriteColor = favoriteColor;
+    this.favoriteFood = favoriteFood;
+}
+
+function Child(firstName, lastName, favoriteColor, favoriteFood){
+    Parent.apply(this, arguments);
+}
+
+// var first = new Child("Ola", "Toś", "red", "pizza");
+// first;
