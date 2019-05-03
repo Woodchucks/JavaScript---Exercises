90da9d91596db2df35ac8e92f9e4d3c1c84dda56
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2019-05-03 20:17:11 +0200

    Create function firstNonRepeatedChar.js

---
+++ function firstNonRepeatedChar.js
@@ -0,0 +1,9 @@
+function firstNonRepeatedChar(string) {
+  for (var i = 0; i < string.length; i++) {
+    var c = string.charAt(i);
+    if (string.indexOf(c) == i && string.indexOf(c, i + 1) == -1) {
+      return c;
+    }
+  }
+  return null;
+}
